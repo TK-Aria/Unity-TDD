62068fe796bbbdc140be8e272cf76fe6139ce638
T.K-Aria <maaenvaohls10040392@outlook.ne.jp>
2019-04-04 22:54:36 +0900

    add project

---
+++ TDD/Runtime/TDD.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TestDrivenDevelopment {
+
+    [Serializable]
+    [CreateAssetMenu(menuName = "ScriptableObject/TestDrivenDevelopment/TDDObject")]
+    public class TDDObject : ScriptableObject    
+    {
+        public virtual void Initialize() { Debug.Log("[ TDD ] : Initialize"); }
+        public virtual void Finalize() { Debug.Log("[ TDD ] : Finalize"); }
+        public virtual void Update() { Debug.Log("[ TDD ] : Update"); }
+
+        public static TDDObject Create()
+        {
+            return ScriptableObject.CreateInstance<TDDObject>();
+        }
+
+        public static TDDObject Load(string key)
+        {
+            return Resources.Load<TDDObject>("TDDObject/" + key);
+        }
+
+        public static TDDObject Instantiate(string key)
+        {
+            return UnityEngine.Object.Instantiate<TDDObject>(Load(key));
+        }
+    }
+
+}
---
+++ TDD/Runtime/TDDComponent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace TestDrivenDevelopment
+{
+
+    public class TDDComponent : MonoBehaviour
+    {
+
+        #region Field
+
+        [SerializeField]
+        private TDDObject _TDDObject;
+
+        #endregion // Field End.
+
+        void Start()
+        {
+            _TDDObject.Initialize();
+        }
+
+        void OnDestroy()
+        {
+            _TDDObject.Finalize();
+        }
+
+        void Update()
+        {
+            _TDDObject.Update();
+        }
+    }
+
+}
